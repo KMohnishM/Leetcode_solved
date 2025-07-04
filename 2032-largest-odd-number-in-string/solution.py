17ba40e30bf03929600c85c5a05a56a9106cea6c
KmohnishM <kmohnishm@gmail.com>
2025-07-04 13:17:36 +0000

    Sync LeetCode submission Runtime - 39 ms (41.48%), Memory - 17.1 MB (19.60%)

--- 2032-largest-odd-number-in-string/solution.py
+++ 2032-largest-odd-number-in-string/solution.py
@@ -1,8 +1,6 @@
 class Solution:
-    def largestOddNumber(self, num):
-        # walk from the rightmost digit to the left
+    def largestOddNumber(self,num):
         for i in range(len(num) - 1, -1, -1):
-            if (ord(num[i]) - 48) & 1:          # faster odd-check than int()
-                return num[: i + 1]             # longest odd-ending prefix
-        return ""                               # no odd digit found
-
+            if int(num[i]) % 2 == 1:  
+                return num[:i + 1]    
+        return ""  
