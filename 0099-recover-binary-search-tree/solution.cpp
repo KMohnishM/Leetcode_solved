d0845eadafd38e118c5272833523c63493da5c4d
KmohnishM <kmohnishm@gmail.com>
2026-02-26 16:20:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 60.8 MB (78.90%)

---
+++ 0099-recover-binary-search-tree/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+
+        if (prev && prev->val > root->val) {
+            if (!first)
+                first = prev;
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val, second->val);
+    }
+};
