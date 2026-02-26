30e299e28ea48ea5cad9d80ba21a751282b16f61
KmohnishM <kmohnishm@gmail.com>
2026-02-26 06:10:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (14.08%)

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int ans = 0;
+    
+        while (s != "1") {
+            if (s.back() == '0') {
+                
+                s.pop_back();
+                ans++;
+            } 
+            else {
+                
+                int i = s.size() - 1;
+
+                
+                while (i >= 0 && s[i] == '1') {
+                    s[i] = '0';
+                    i--;
+                }
+
+                if (i < 0) {
+                    
+                    s = '1' + s;
+                } else {
+                    s[i] = '1';
+                }
+
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
