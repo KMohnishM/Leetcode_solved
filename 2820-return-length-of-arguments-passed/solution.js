4037ab25911b50d6c9610beef2e47d40a0cdb18a
KmohnishM <kmohnishm@gmail.com>
2026-03-05 14:10:50 +0000

    Sync LeetCode submission Runtime - 40 ms (72.15%), Memory - 54.9 MB (5.85%)

---
+++ 2820-return-length-of-arguments-passed/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // var a=0;
+    
+    // console.log(args.length)
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
