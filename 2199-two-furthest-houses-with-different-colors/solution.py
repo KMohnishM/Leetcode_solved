c1a16e1295448483061a0a7704d8a35c635cb978
KmohnishM <kmohnishm@gmail.com>
2026-04-20 01:31:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (60.31%)

---
+++ 2199-two-furthest-houses-with-different-colors/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxDistance(self, colors):
+        n = len(colors)
+        
+        
+        max_dist = 0
+        for j in range(n-1, -1, -1):
+            if colors[j] != colors[0]:
+                max_dist = j
+                break
+        
+        
+        for i in range(n):
+            if colors[i] != colors[n-1]:
+                max_dist = max(max_dist, (n-1) - i)
+                break
+        
+        return max_dist
