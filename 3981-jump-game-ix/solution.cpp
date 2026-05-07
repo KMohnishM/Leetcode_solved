c1cf40ab2be701da2cb63f5cf294aac6861c07e0
KmohnishM <kmohnishm@gmail.com>
2026-05-07 03:45:34 +0000

    Sync LeetCode submission Runtime - 19 ms (30.90%), Memory - 225.8 MB (27.42%)

---
+++ 3981-jump-game-ix/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> prefMax(n), suffMin(n);
+
+        prefMax[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            prefMax[i] = max(prefMax[i - 1], nums[i]);
+        }
+
+        suffMin[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            suffMin[i] = min(suffMin[i + 1], nums[i]);
+        }
+
+        vector<int> ans(n);
+
+        int start = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(i == n - 1 || prefMax[i] <= suffMin[i + 1]) {
+
+                int mx = prefMax[i];
+
+                for(int j = start; j <= i; j++) {
+                    ans[j] = mx;
+                }
+
+                start = i + 1;
+            }
+        }
+
+        return ans;
+    }
+};
