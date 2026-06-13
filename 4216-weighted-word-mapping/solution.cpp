468d1db9410e9338435300db5887027839533bce
KmohnishM <kmohnishm@gmail.com>
2026-06-13 02:57:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.7 MB (55.82%)

---
+++ 4216-weighted-word-mapping/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string result;
+        for (const string& word : words) {
+            int sum = 0;
+            for (char c : word) {
+                sum += weights[c - 'a'];
+            }
+            int mod = sum % 26;
+            result.push_back('z' - mod); 
+        }
+        return result;
+    }
+};
