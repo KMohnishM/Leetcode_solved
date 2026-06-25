269a16e2c0b89021995f6f6cc5d7bd5132d29464
KmohnishM <kmohnishm@gmail.com>
2026-06-25 06:01:51 +0000

    Sync LeetCode submission Runtime - 8 ms (96.37%), Memory - 25.1 MB (71.16%)

---
+++ 0049-group-anagrams/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> store;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+
+            store[temp].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+
+        for (auto &it : store) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
