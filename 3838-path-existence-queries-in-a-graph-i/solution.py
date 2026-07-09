c3b45ac235e3cde8c27edf01470e2cc6803f0d7a
KmohnishM <kmohnishm@gmail.com>
2026-07-09 05:23:30 +0000

    Sync LeetCode submission Runtime - 68 ms (81.07%), Memory - 50.3 MB (28.94%)

---
+++ 3838-path-existence-queries-in-a-graph-i/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+
+        comp = [0] * n
+        curr = 0
+
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                curr += 1
+            comp[i] = curr
+
+        return [comp[u] == comp[v] for u, v in queries]
