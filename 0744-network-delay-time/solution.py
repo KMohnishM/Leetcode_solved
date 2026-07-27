2df299e3e2f9e43bab71db08220132efb289a9ad
KmohnishM <kmohnishm@gmail.com>
2026-07-27 06:55:21 +0000

    Sync LeetCode submission Runtime - 331 ms (79.76%), Memory - 21.2 MB (78.26%)

---
+++ 0744-network-delay-time/solution.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph=defaultdict(list)
+        for u,v,w in times:
+            graph[u].append((v,w))
+        dist=[float('inf')]*(n+1)
+        dist[k]=0
+        heap=[(0,k)]
+        while heap:
+            d,u=heapq.heappop(heap)
+            if d > dist[u]:
+              continue
+
+            for v, w in graph[u]:
+              if d + w < dist[v]:
+                dist[v] = d + w
+                heapq.heappush(heap, (dist[v], v))    
+        ans = max(dist[1:])
+        
+        
+        return ans if ans < float('inf') else -1      
+        
