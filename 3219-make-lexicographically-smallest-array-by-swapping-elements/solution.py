c838b1ad132ddc33cc0baf679ae22946b56e375f
KmohnishM <kmohnishm@gmail.com>
2026-08-29 05:42:41 +0000

    Sync LeetCode submission Runtime - 398 ms (21.64%), Memory - 46.4 MB (84.33%)

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        n = len(nums)
+        arr = sorted((num, i) for i, num in enumerate(nums))
+        ans = [0] * n
+        i = 0
+
+        while i < n:
+            j = i
+            while j + 1 < n and arr[j + 1][0] - arr[j][0] <= limit:
+                j += 1
+            indices = sorted(arr[k][1] for k in range(i, j + 1))
+            values = [arr[k][0] for k in range(i, j + 1)]
+            for idx, val in zip(indices, values):
+                ans[idx] = val
+
+            i = j + 1
+
+        return ans
